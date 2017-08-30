18bb9000d75ae7a4e7c977558975159f2fac3884
nnsnodnb <ahr63_gej@me.com>
2017-08-30 17:16:59 +0900

    Initial commit

---
+++ NowPlaying/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NowPlaying
+//
+//  Created by Oka Yuya on 2017/08/30.
+//  Copyright © 2017年 Oka Yuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
