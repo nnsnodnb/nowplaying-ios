a8f74b8d2706498df165ebdefa9aaa7e67c24c3c
nnsnodnb <ahr63_gej@me.com>
2017-08-30 17:38:37 +0900

    Access & Show music library

--- NowPlaying/ViewController.swift
+++ NowPlaying/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
+
+    fileprivate let audioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTapButton(_ sender: Any) {
+        let pickerViewController = MPMediaPickerController(mediaTypes: .music)
+        pickerViewController.delegate = self
+        pickerViewController.allowsPickingMultipleItems = false
+        present(pickerViewController, animated: true, completion: nil)
+    }
 }
 
+// MARK: - MPMediaPickerControllerDelegate
+
+extension ViewController : MPMediaPickerControllerDelegate {
+
+    func mediaPicker(_ mediaPicker: MPMediaPickerController, didPickMediaItems mediaItemCollection: MPMediaItemCollection) {
+        
+    }
+
+    func mediaPickerDidCancel(_ mediaPicker: MPMediaPickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
