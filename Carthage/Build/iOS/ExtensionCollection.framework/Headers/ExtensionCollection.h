e3d5cc43bc7d58c8a40c628b9e8cd18114fbed52
nnsnodnb <ahr63_gej@me.com>
2017-09-02 19:01:25 +0900

    Add ExtensionCollection for Carthage

---
+++ Carthage/Build/iOS/ExtensionCollection.framework/Headers/ExtensionCollection.h
@@ -0,0 +1,19 @@
+//
+//  ExtensionCollection.h
+//  ExtensionCollection
+//
+//  Created by Oka Yuya on 2017/08/01.
+//  Copyright © 2017年 Oka Yuya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExtensionCollection.
+FOUNDATION_EXPORT double ExtensionCollectionVersionNumber;
+
+//! Project version string for ExtensionCollection.
+FOUNDATION_EXPORT const unsigned char ExtensionCollectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExtensionCollection/PublicHeader.h>
+
+
