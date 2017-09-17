21825e8f0ba5d5dfbf797ca21506f3eb1d7ce908
nnsnodnb <ahr63_gej@me.com>
2017-09-18 05:39:37 +0900

    Add Twitter kit

--- NowPlaying/Scripts/AppDelegate.swift
+++ NowPlaying/Scripts/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import TwitterKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         UIApplication.shared.beginReceivingRemoteControlEvents()
+        let env = ProcessInfo.processInfo.environment
+        Twitter.sharedInstance().start(withConsumerKey: env["CONSUMER_KEY"]!, consumerSecret: env["CONSUMER_SECRET"]!)
         return true
     }
 
