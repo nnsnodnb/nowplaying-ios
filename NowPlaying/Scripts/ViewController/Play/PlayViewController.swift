6ee8ec3c6373e1f4f7927d0a9f59181b0f55b0ce
nnsnodnb <ahr63_gej@me.com>
2017-09-24 19:04:47 +0900

    Add UIAlertController on not login

--- NowPlaying/Scripts/ViewController/Play/PlayViewController.swift
+++ NowPlaying/Scripts/ViewController/Play/PlayViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MediaPlayer
+import TwitterKit
 
 class PlayViewController: UIViewController {
 
@@ -86,6 +87,12 @@ class PlayViewController: UIViewController {
     }
 
     @IBAction func onTapTwitterButton(_ sender: Any) {
+        if Twitter.sharedInstance().sessionStore.session() == nil {
+            let alert = UIAlertController(title: nil, message: "設定からログインしてください", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "閉じる", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            return
+        }
         let tweetViewController = TweetViewController()
         tweetViewController.tweetText = "\(song?.title ?? "") by \(song?.artist ?? "") #NowPlaying"
         tweetViewController.shareImage = userDefaults.bool(forKey: UserDefaultsKey.isWithImage.rawValue) ? artworkImageView.image : nil
