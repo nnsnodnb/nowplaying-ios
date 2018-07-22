7cd22e31ee3e84f425f28d42f2e05d792c85a2c6
nnsnodnb <ahr63_gej@me.com>
2018-07-22 17:25:17 +0900

    Update In-App Purchases for auto tweet

--- NowPlaying/Scripts/ViewController/Setting/TwitterSettingViewController.swift
+++ NowPlaying/Scripts/ViewController/Setting/TwitterSettingViewController.swift
@@ -57,6 +57,20 @@ class TwitterSettingViewController: SNSSettingBaseViewController {
         productRequest = PaymentManager.shared.startProductRequest(productId)
     }
 
+    private func completePuchaseAutoTweet() {
+        UserDefaults.set(true, forKey: .isAutoTweetPurchase)
+        DispatchQueue.main.async { [weak self] in
+            SVProgressHUD.dismiss()
+            guard let wself = self else { return }
+            wself.isProces = false
+            let purchaseButtonRow: ButtonRow = wself.form.rowBy(tag: "auto_tweet_purchase")!
+            let autoTweetSwitchRow: SwitchRow = wself.form.rowBy(tag: "auto_tweet_switch")!
+            purchaseButtonRow.hidden = Condition(booleanLiteral: true)
+            purchaseButtonRow.evaluateHidden()
+            autoTweetSwitchRow.evaluateHidden()
+        }
+    }
+
     // MARK: - Override method
 
     override func setupForm() {
@@ -172,25 +186,61 @@ extension TwitterSettingViewController: PaymentManagerProtocol {
 
     func finish(request: SKProductsRequest, products: [SKProduct]) {
         autoTweetProduct = products.first
+        SVProgressHUD.dismiss()
     }
 
     func finish(request: SKRequest, didFailWithError: Error) {
-
+        SVProgressHUD.showError(withStatus: "通信エラーが発生しました")
+        SVProgressHUD.dismiss(withDelay: 0.3)
     }
 
     func finish(success paymentTransaction: SKPaymentTransaction) {
-
+        DispatchQueue.main.async {
+            SVProgressHUD.show()
+        }
+        guard let receiptUrl = Bundle.main.appStoreReceiptURL, let receiptData = try? Data(contentsOf: receiptUrl, options: .uncached) else { return }
+        let request = PurchaseRequest(receiptData: receiptData.base64EncodedString())
+        request.send { [weak self] (result) in
+            DispatchQueue.main.async {
+                SVProgressHUD.dismiss()
+            }
+            switch result {
+            case .success(let response):
+                guard let body = response.body, let status = body["status"] as? Int,
+                    let wself = self, status == 0 else {
+                        return
+                }
+                wself.completePuchaseAutoTweet()
+            case .failure:
+                SVProgressHUD.showError(withStatus: "検証に失敗しました")
+                SVProgressHUD.dismiss(withDelay: 0.3)
+            }
+        }
     }
 
     func finishPayment(failed paymentTransaction: SKPaymentTransaction) {
-
+        isProces = false
+        DispatchQueue.main.async {
+            SVProgressHUD.showError(withStatus: "購入に失敗しました")
+            SVProgressHUD.dismiss(withDelay: 0.3)
+        }
     }
 
     func finishRestore(queue: SKPaymentQueue) {
-
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            SVProgressHUD.dismiss {
+                SVProgressHUD.showInfo(withStatus: "復元に成功しました")
+            }
+        }
+        completePuchaseAutoTweet()
+        isProces = false
     }
 
     func finishRestore(queue: SKPaymentQueue, restoreCompletedTransactionsFailedWithError: Error) {
-
+        isProces = false
+        DispatchQueue.main.async {
+            SVProgressHUD.showError(withStatus: "復元に失敗しました")
+            SVProgressHUD.dismiss(withDelay: 0.3)
+        }
     }
 }
