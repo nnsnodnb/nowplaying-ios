7b7e81ef2724cf1bdf9e5faa62858e1d29a7fd94
nnsnodnb <ahr63_gej@me.com>
2019-06-06 23:00:56 +0900

    Update project tree

---
+++ NowPlaying/Scripts/Screens/Setting/MastodonSetting/MastodonSettingViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  MastodonSettingViewModel.swift
+//  NowPlaying
+//
+//  Created by Yuya Oka on 2019/06/06.
+//  Copyright © 2019 Oka Yuya. All rights reserved.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
---
+++ NowPlaying/Scripts/Screens/Setting/TwitterSetting/TwitterSettingViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  TwitterSettingViewModel.swift
+//  NowPlaying
+//
+//  Created by Yuya Oka on 2019/06/06.
+//  Copyright © 2019 Oka Yuya. All rights reserved.
+//
+
+import Foundation
