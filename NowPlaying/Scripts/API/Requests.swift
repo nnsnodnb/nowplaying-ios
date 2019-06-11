77e86c38b25c482a84f22b863bfa0418d9a12a8a
nnsnodnb <ahr63_gej@me.com>
2019-06-12 04:07:25 +0900

    Update requests

---
+++ NowPlaying/Scripts/API/Requests.swift
@@ -0,0 +1,11 @@
+//
+//  Requests.swift
+//  NowPlaying
+//
+//  Created by Yuya Oka on 2019/06/12.
+//  Copyright © 2019 Oka Yuya. All rights reserved.
+//
+
+import APIKit
+
+protocol MastodonRequest: Request {}
