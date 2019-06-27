e323d43a716626b8c673407cfc3c00412eea3b61
nnsnodnb <nnsnodnb@gmail.com>
2019-06-27 19:25:41 +0900

    Add artwork preiview viewcontroller

---
+++ NowPlaying/Scripts/Screens/Preview/ArtworkPreviewViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ArtworkPreviewViewController.swift
+//  NowPlaying
+//
+//  Created by Oka Yuya on 2019/06/27.
+//  Copyright © 2019 Oka Yuya. All rights reserved.
+//
+
+import UIKit
+
+final class ArtworkPreviewViewController: UIViewController {
+
+    @IBOutlet private weak var previewImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
