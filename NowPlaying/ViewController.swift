a3ace42bb7f03e625e51ed697ad43fb8f556c64c
nnsnodnb <nnsnodnb@gmail.com>
2020-01-23 16:28:55 +0900

    Add initial project

---
+++ NowPlaying/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NowPlaying
+//
+//  Created by Oka Yuya on 2020/01/23.
+//  Copyright © 2020 Yuya Oka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
