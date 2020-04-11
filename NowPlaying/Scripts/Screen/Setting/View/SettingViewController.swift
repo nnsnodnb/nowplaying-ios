b07860d05b3134ad44da3e37e00ba9777f5e9403
nnsnodnb <nnsnodnb@gmail.com>
2020-04-11 17:46:55 +0900

    [WIP] Update settings view

--- NowPlaying/Scripts/Screen/Setting/View/SettingViewController.swift
+++ NowPlaying/Scripts/Screen/Setting/View/SettingViewController.swift
@@ -6,13 +6,33 @@
 //  Copyright © 2020 Yuya Oka. All rights reserved.
 //
 
+import Eureka
+import RxCocoa
+import RxSwift
 import UIKit
 
-final class SettingViewController: UIViewController {
+final class SettingViewController: FormViewController {
+
+    private let disposeBag = DisposeBag()
+
+    private(set) var viewModel: SettingViewModelType!
 
     // MARK: - Life cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension SettingViewController: SettingViewer {}
+
+extension SettingViewController {
+
+    struct Dependency {
+        let viewModel: SettingViewModelType
+    }
+
+    func inject() {
+        
     }
 }
