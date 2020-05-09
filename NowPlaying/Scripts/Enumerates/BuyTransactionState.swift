3c394903a840ff79b616c5d0e0c8faf25fbedc58
nnsnodnb <nnsnodnb@gmail.com>
2020-05-09 15:41:49 +0900

    Add buyProduct & restore function

---
+++ NowPlaying/Scripts/Enumerates/BuyTransactionState.swift
@@ -0,0 +1,16 @@
+//
+//  BuyTransactionState.swift
+//  NowPlaying
+//
+//  Created by Oka Yuya on 2020/05/09.
+//  Copyright © 2020 Yuya Oka. All rights reserved.
+//
+
+import Foundation
+import StoreKit
+
+enum BuyTransactionState {
+
+    case purchasing
+    case purchased
+}
