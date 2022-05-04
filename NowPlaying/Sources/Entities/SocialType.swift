8137d75ee33b82503d584d0b032e97c2ce0c399b
Yuya Oka <nnsnodnb@gmail.com>
2022-05-04 19:46:38 +0900

    Apply image

--- NowPlaying/Sources/Entities/SocialType.swift
+++ NowPlaying/Sources/Entities/SocialType.swift
@@ -5,9 +5,19 @@
 //  Created by Yuya Oka on 2022/05/04.
 //
 
-import Foundation
+import UIKit
 
 enum SocialType: String {
     case twitter
     case mastodon
+
+    // MARK: - Properties
+    var image: UIImage {
+        switch self {
+        case .twitter:
+            return Asset.Assets.icTwitter.image
+        case .mastodon:
+            return Asset.Assets.icMastodon.image
+        }
+    }
 }
