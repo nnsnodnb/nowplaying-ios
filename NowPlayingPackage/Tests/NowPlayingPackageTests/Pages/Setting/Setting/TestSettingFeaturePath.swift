cb0559c25ef8aa21a399d9f7592c79aa7a61adf0
nnsnodnb <nnsnodnb@gmail.com>
2026-03-09 22:58:49 +0900

    Add SettinFeature's test

---
+++ NowPlayingPackage/Tests/NowPlayingPackageTests/Pages/Setting/Setting/TestSettingFeaturePath.swift
@@ -0,0 +1,32 @@
+//
+//  TestSettingFeaturePath.swift
+//  NowPlayingPackage
+//
+//  Created by Yuya Oka on 2026/03/09.
+//
+
+import ComposableArchitecture
+@testable import NowPlayingPackage
+import Testing
+
+@MainActor
+struct TestSettingFeaturePath {
+  @Test
+  func testPathElementTwitterSettingDelegatePushTwitterAccountManage() async throws {
+    var path = StackState<SettingFeature.Path.State>()
+    path[id: 0] = .twitterSetting(.init())
+
+    let store = TestStore(
+      initialState: SettingFeature.State(
+        path: path,
+      ),
+      reducer: {
+        SettingFeature()
+      },
+    )
+
+    await store.send(.path(.element(id: 0, action: .twitterSetting(.delegate(.pushTwitterAccountManage))))) {
+      $0.path.append(.twitterAccountManage(.init()))
+    }
+  }
+}
