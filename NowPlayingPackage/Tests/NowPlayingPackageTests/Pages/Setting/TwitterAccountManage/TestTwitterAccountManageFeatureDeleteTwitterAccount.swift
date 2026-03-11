82360109677a04680623768bca15a314f38c49ca
nnsnodnb <nnsnodnb@gmail.com>
2026-03-11 12:58:06 +0900

    Add empty test file

---
+++ NowPlayingPackage/Tests/NowPlayingPackageTests/Pages/Setting/TwitterAccountManage/TestTwitterAccountManageFeatureDeleteTwitterAccount.swift
@@ -0,0 +1,17 @@
+//
+//  TestTwitterAccountManageFeatureDeleteTwitterAccount.swift
+//  NowPlayingPackage
+//
+//  Created by Yuya Oka on 2026/03/11.
+//
+
+import ComposableArchitecture
+import Testing
+
+@MainActor
+struct TestTwitterAccountManageFeatureDeleteTwitterAccount {
+  @Test
+  func testIt() async throws {
+    // TODO: 実装
+  }
+}
