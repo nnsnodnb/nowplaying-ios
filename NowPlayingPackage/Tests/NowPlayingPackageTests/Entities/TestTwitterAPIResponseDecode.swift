ff900902b1be99dfe5bc28e5af8652668a9acd9e
nnsnodnb <nnsnodnb@gmail.com>
2026-03-18 22:13:30 +0900

    Add TwitterAPIResponse<TwitterMedia> decode test

--- NowPlayingPackage/Tests/NowPlayingPackageTests/Entities/TestTwitterAPIResponseDecode.swift
+++ NowPlayingPackage/Tests/NowPlayingPackageTests/Entities/TestTwitterAPIResponseDecode.swift
@@ -11,7 +11,7 @@ import Testing
 
 struct TestTwitterAPIResponseDecode {
   @Test
-  func testIt() throws {
+  func testTwitterProfileDecode() throws {
     let profileImageURLString = "https://pbs.twimg.com/profile_images/1593438620769488897/3kV4Mtvq_normal.jpg"
     let jsonObject = [
       "data": [
@@ -26,10 +26,40 @@ struct TestTwitterAPIResponseDecode {
     let jsonDecoder = JSONDecoder()
     let response = try jsonDecoder.decode(TwitterAPIResponse<TwitterProfile>.self, from: data)
 
-    #expect(response.data.id == "1137201750")
+    #expect(response.data.id == .init("1137201750"))
     #expect(response.data.name == "小泉ひやかし🌻")
     #expect(response.data.username == "nnsnodnb")
     let expectedProfileImageURLString = "https://pbs.twimg.com/profile_images/1593438620769488897/3kV4Mtvq.jpg"
     #expect(response.data.profileImageURL == URL(string: expectedProfileImageURLString))
+  }
+
+  @Test
+  func testTwitterMediaDecode() async throws {
+    let now = Date.now
+    try withDependencies {
+      $0.date = .constant(now)
+    } operation: {
+      let jsonObject = [
+        "data": [
+          "media_key": "3_2034250625912016896",
+          "id": "2034250625912016896",
+          "size": 37523,
+          "image": [
+            "w": 600,
+            "image_type": "image/jpeg",
+            "h": 600
+          ],
+          "expires_after_secs": 86400,
+        ],
+      ]
+
+      let data = try JSONSerialization.data(withJSONObject: jsonObject, options: .init())
+      let jsonDecoder = JSONDecoder()
+      let response = try jsonDecoder.decode(TwitterAPIResponse<TwitterMedia>.self, from: data)
+
+      #expect(response.data.id == .init("2034250625912016896"))
+      #expect(response.data.expiresAfterSecs == 86_400)
+      #expect(response.data.isExpired == false)
+    }
   }
 }
