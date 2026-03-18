7e18fe328e888268ca1a9f714a8827eea835c0a7
nnsnodnb <nnsnodnb@gmail.com>
2026-03-18 21:12:43 +0900

    Add tests

---
+++ NowPlayingPackage/Tests/NowPlayingPackageTests/Pages/Tweet/TestTweetFeatureInternalAction.swift
@@ -0,0 +1,303 @@
+//
+//  TestTweetFeatureInternalAction.swift
+//  NowPlayingPackage
+//
+//  Created by Yuya Oka on 2026/03/18.
+//
+
+import ComposableArchitecture
+import DependenciesTestSupport
+@testable import NowPlayingPackage
+import StubKit
+import Testing
+
+@MainActor
+struct TestTweetFeatureInternalAction {
+  @Test(
+    .dependency(\.date, .constant(.now))
+  )
+  func testUploadImageDataSuccess() async throws {
+    let mainQueue = DispatchQueue.test
+    let twitterAccount = try Stub.make(TwitterAccount.self)
+    let twitterMedia = try Stub.make(TwitterMedia.self)
+
+    await withDependencies {
+      $0.mainQueue = mainQueue.eraseToAnyScheduler()
+      $0.twitterAPI.uploadMedia = { _, _ in twitterMedia }
+      $0.twitterAPI.post = { _, _, _ in }
+    } operation: {
+      let store = TestStore(
+        initialState: TweetFeature.State(
+          twitterAccounts: [twitterAccount],
+          title: "曲名",
+          artist: "アーティスト名",
+          album: nil,
+          artwork: .init(systemSymbol: .photoFill),
+          capturedImage: .init(systemSymbol: .photo),
+          attachmentImage: .init(systemSymbol: .photoFill),
+          postableTwitterAccount: twitterAccount,
+          text: "曲名 / アーティスト #NowPlaying",
+          isLoading: true,
+        ),
+        reducer: {
+          TweetFeature()
+        },
+      )
+
+      guard let imageData = UIImage(systemSymbol: .photoFill).jpegData(compressionQuality: 0.3) else {
+        Issue.record("imageData must be not nil.")
+        return
+      }
+      await store.send(.internalAction(.uploadImageData(.init("stub_access_token"), imageData)))
+      await store.receive(\.internalAction.post) {
+        $0.temporaryMedia = twitterMedia
+      }
+      await store.receive(\.internalAction.posted) {
+        $0.isLoading = false
+        $0.showSuccess = true
+      }
+      await mainQueue.advance(by: .milliseconds(500))
+      await store.receive(\.internalAction.dismiss) {
+        $0.showSuccess = false
+      }
+      await store.receive(\.close)
+    }
+  }
+
+  @Test(
+    .dependency(\.date, .constant(.now))
+  )
+  func testUploadImageDataFailure() async throws {
+    let twitterAccount = try Stub.make(TwitterAccount.self)
+
+    await withDependencies {
+      $0.secureKeyValueStore.twitterAccounts = { [twitterAccount] }
+      $0.twitterAPI.uploadMedia = { _, _ in throw TwitterAPIClient.Error.internalError }
+    } operation: {
+      let store = TestStore(
+        initialState: TweetFeature.State(
+          twitterAccounts: [twitterAccount],
+          title: "曲名",
+          artist: "アーティスト名",
+          album: nil,
+          artwork: .init(systemSymbol: .photoFill),
+          capturedImage: .init(systemSymbol: .photo),
+          attachmentImage: .init(systemSymbol: .photoFill),
+          postableTwitterAccount: twitterAccount,
+          text: "曲名 / アーティスト #NowPlaying",
+          isLoading: true,
+        ),
+        reducer: {
+          TweetFeature()
+        },
+      )
+
+      guard let imageData = UIImage(systemSymbol: .photoFill).jpegData(compressionQuality: 0.3) else {
+        Issue.record("imageData must be not nil.")
+        return
+      }
+      await store.send(.internalAction(.uploadImageData(.init("stub_access_token"), imageData)))
+      await store.receive(\.internalAction.postFailure) {
+        $0.isLoading = false
+        $0.alert = AlertState(
+          title: {
+            TextState("画像のアップロードに失敗しました")
+          },
+          actions: {
+            ButtonState(
+              action: .close,
+              label: {
+                TextState("閉じる")
+              },
+            )
+          },
+        )
+      }
+      await store.receive(\.internalAction.fetchTwitterAccounts)
+      await store.receive(\.internalAction.refreshTwitterAccounts)
+    }
+  }
+
+  @Test(arguments: [true, false])
+  func testPostSuccess(hasMedia: Bool) async throws {
+    let mainQueue = DispatchQueue.test
+    let twitterAccount = try Stub.make(TwitterAccount.self)
+    let twitterMedia: TwitterMedia? = try withDependencies {
+      $0.date = .constant(.now)
+    } operation: {
+      if hasMedia {
+        return try Stub.make(TwitterMedia.self)
+      } else {
+        return nil
+      }
+    }
+
+    await withDependencies {
+      $0.mainQueue = mainQueue.eraseToAnyScheduler()
+      $0.twitterAPI.post = { _, _, _ in }
+    } operation: {
+      let store = TestStore(
+        initialState: TweetFeature.State(
+          twitterAccounts: [twitterAccount],
+          title: "曲名",
+          artist: "アーティスト名",
+          album: nil,
+          artwork: .init(systemSymbol: .photoFill),
+          capturedImage: .init(systemSymbol: .photo),
+          postableTwitterAccount: twitterAccount,
+          text: "曲名 / アーティスト #NowPlaying",
+          isLoading: true,
+        ),
+        reducer: {
+          TweetFeature()
+        },
+      )
+
+      if hasMedia {
+        await store.send(.internalAction(.post(.init("stub_access_token"), twitterMedia))) {
+          $0.temporaryMedia = twitterMedia
+        }
+      } else {
+        await store.send(.internalAction(.post(.init("stub_access_token"), twitterMedia)))
+      }
+      await store.receive(\.internalAction.posted) {
+        $0.isLoading = false
+        $0.showSuccess = true
+      }
+      await mainQueue.advance(by: .milliseconds(500))
+      await store.receive(\.internalAction.dismiss) {
+        $0.showSuccess = false
+      }
+      await store.receive(\.close)
+    }
+  }
+
+  @Test
+  func testPostFailure() async throws {
+    let twitterAccount = try Stub.make(TwitterAccount.self)
+
+    await withDependencies {
+      $0.secureKeyValueStore.twitterAccounts = { [twitterAccount] }
+      $0.twitterAPI.post = { _, _, _ in throw TwitterAPIClient.Error.internalError }
+    } operation: {
+      let store = TestStore(
+        initialState: TweetFeature.State(
+          twitterAccounts: [twitterAccount],
+          title: "曲名",
+          artist: "アーティスト名",
+          album: nil,
+          artwork: .init(systemSymbol: .photoFill),
+          capturedImage: .init(systemSymbol: .photo),
+          postableTwitterAccount: twitterAccount,
+          text: "曲名 / アーティスト #NowPlaying",
+          isLoading: true,
+        ),
+        reducer: {
+          TweetFeature()
+        },
+      )
+
+      await store.send(.internalAction(.post(.init("stub_access_token"), nil)))
+      await store.receive(\.internalAction.postFailure) {
+        $0.isLoading = false
+        $0.alert = AlertState(
+          title: {
+            TextState("ポストに失敗しました")
+          },
+          actions: {
+            ButtonState(
+              action: .close,
+              label: {
+                TextState("閉じる")
+              },
+            )
+          },
+        )
+      }
+      await store.receive(\.internalAction.fetchTwitterAccounts)
+      await store.receive(\.internalAction.refreshTwitterAccounts)
+    }
+  }
+
+  @Test
+  func testPosted() async throws {
+    let mainQueue = DispatchQueue.test
+    let twitterAccount = try Stub.make(TwitterAccount.self)
+
+    await withDependencies {
+      $0.mainQueue = mainQueue.eraseToAnyScheduler()
+    } operation: {
+      let store = TestStore(
+        initialState: TweetFeature.State(
+          twitterAccounts: [twitterAccount],
+          title: "曲名",
+          artist: "アーティスト名",
+          album: nil,
+          artwork: .init(systemSymbol: .photoFill),
+          capturedImage: .init(systemSymbol: .photo),
+          postableTwitterAccount: twitterAccount,
+          text: "曲名 / アーティスト #NowPlaying",
+          isLoading: true,
+        ),
+        reducer: {
+          TweetFeature()
+        },
+      )
+
+      await store.send(.internalAction(.posted)) {
+        $0.isLoading = false
+        $0.showSuccess = true
+      }
+      await mainQueue.advance(by: .milliseconds(500))
+      await store.receive(\.internalAction.dismiss) {
+        $0.showSuccess = false
+      }
+      await store.receive(\.close)
+    }
+  }
+
+  @Test
+  func test_PostFailure() async throws {
+    let twitterAccount = try Stub.make(TwitterAccount.self)
+
+    await withDependencies {
+      $0.secureKeyValueStore.twitterAccounts = { [twitterAccount] }
+    } operation: {
+      let store = TestStore(
+        initialState: TweetFeature.State(
+          twitterAccounts: [twitterAccount],
+          title: "曲名",
+          artist: "アーティスト名",
+          album: nil,
+          artwork: .init(systemSymbol: .photoFill),
+          capturedImage: .init(systemSymbol: .photo),
+          postableTwitterAccount: twitterAccount,
+          text: "曲名 / アーティスト #NowPlaying",
+          isLoading: true,
+        ),
+        reducer: {
+          TweetFeature()
+        },
+      )
+
+      await store.send(.internalAction(.postFailure("テスト"))) {
+        $0.isLoading = false
+        $0.alert = AlertState(
+          title: {
+            TextState("テスト")
+          },
+          actions: {
+            ButtonState(
+              action: .close,
+              label: {
+                TextState("閉じる")
+              },
+            )
+          },
+        )
+      }
+      await store.receive(\.internalAction.fetchTwitterAccounts)
+      await store.receive(\.internalAction.refreshTwitterAccounts)
+    }
+  }
+}
--- NowPlayingPackage/Tests/NowPlayingPackageTests/Pages/Tweet/TestTweetFeatureRemoveAttachmentImage.swift
+++ NowPlayingPackage/Tests/NowPlayingPackageTests/Pages/Tweet/TestTweetFeatureRemoveAttachmentImage.swift
@@ -6,13 +6,19 @@
 //
 
 import ComposableArchitecture
+import DependenciesTestSupport
 @testable import NowPlayingPackage
+import StubKit
 import Testing
 
 @MainActor
 struct TestTweetFeatureRemoveAttachmentImage {
-  @Test
+  @Test(
+    .dependency(\.date, .constant(.now))
+  )
   func testIt() async throws {
+    let twitterMedia = try Stub.make(TwitterMedia.self)
+
     let store = TestStore(
       initialState: TweetFeature.State(
         twitterAccounts: [],
@@ -22,6 +28,7 @@ struct TestTweetFeatureRemoveAttachmentImage {
         artwork: nil,
         capturedImage: .init(systemSymbol: .photo),
         attachmentImage: .init(systemSymbol: .photo),
+        temporaryMedia: twitterMedia,
       ),
       reducer: {
         TweetFeature()
@@ -30,6 +37,7 @@ struct TestTweetFeatureRemoveAttachmentImage {
 
     await store.send(.removeAttachmentImage) {
       $0.attachmentImage = nil
+      $0.temporaryMedia = nil
     }
   }
 }
