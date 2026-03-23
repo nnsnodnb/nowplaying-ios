60f17ff270180df5d2e600d0f3dd2932ce3fe304
nnsnodnb <nnsnodnb@gmail.com>
2026-03-23 22:04:56 +0900

    Export RevenueCat

--- NowPlayingPackage/Sources/NowPlayingPackage/Components/ButtonStyle/PressScaleButtonStyle.swift
+++ NowPlayingPackage/Sources/NowPlayingPackage/Components/ButtonStyle/PressScaleButtonStyle.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 public struct PressScaleButtonStyle: ButtonStyle {
-  public func makeBody(configuration: Configuration) -> some View {
+  public func makeBody(configuration: ButtonStyle.Configuration) -> some View {
     configuration.label
       .scaleEffect(configuration.isPressed ? 0.8 : 1)
       .animation(.easeInOut(duration: 0.15), value: configuration.isPressed)
