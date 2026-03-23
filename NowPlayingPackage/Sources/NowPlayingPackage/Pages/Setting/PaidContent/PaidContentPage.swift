3caed142c01bd099011ca85e6a73002c3de9d4a1
nnsnodnb <nnsnodnb@gmail.com>
2026-03-23 21:46:55 +0900

    [wip] Update PaidContentPage

--- NowPlayingPackage/Sources/NowPlayingPackage/Pages/Setting/PaidContent/PaidContentPage.swift
+++ NowPlayingPackage/Sources/NowPlayingPackage/Pages/Setting/PaidContent/PaidContentPage.swift
@@ -13,26 +13,62 @@ public struct PaidContentFeature: Sendable {
   // MARK: - State
   @ObservableState
   public struct State: Equatable {
+    public let isLoading = false
   }
 
   // MARK: - Action
   public enum Action {
+    case purchaseHideAds
   }
 
   // MARK: - Body
   public var body: some ReducerOf<Self> {
-    EmptyReducer()
+    Reduce { state, action in
+      switch action {
+      case .purchaseHideAds:
+        return .none
+      }
+    }
   }
 }
 
 public struct PaidContentPage: View {
   // MARK: - Properties
-  public let store: StoreOf<PaidContentFeature>
+  @Bindable public var store: StoreOf<PaidContentFeature>
 
   // MARK: - Body
   public var body: some View {
-    Text("PaidContent")
+    list
+      .navigationTitle("有料コンテンツ")
       .interactiveDismissDisabled(true)
+      .progress(store.isLoading)
+  }
+
+  private var list: some View {
+    List {
+      section
+    }
+  }
+
+  private var section: some View {
+    Section {
+      hideAdsRow
+    }
+  }
+
+  private var hideAdsRow: some View {
+    ButtonRow(
+      action: {
+        store.send(.purchaseHideAds)
+      },
+      title: "バナー広告の削除",
+      icon: {
+        Image(systemSymbol: .eyeSlash)
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .foregroundStyle(.red)
+      },
+    )
   }
 }
 
