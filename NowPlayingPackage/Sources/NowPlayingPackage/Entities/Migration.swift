3bd306469bf6a543bfb7c39dec1d5410df93ef36
nnsnodnb <nnsnodnb@gmail.com>
2026-05-16 17:53:22 +0900

    Fix extension_access_modifier

--- NowPlayingPackage/Sources/NowPlayingPackage/Entities/Migration.swift
+++ NowPlayingPackage/Sources/NowPlayingPackage/Entities/Migration.swift
@@ -10,8 +10,8 @@ import Foundation
 public struct Migration {}
 
 // MARK: - V320
-extension Migration {
-  public struct V320 {
+public extension Migration {
+  struct V320 {
     // MARK: - Properties
     public let twitterAccount: TwitterAccount
     public let refreshToken: TwitterOAuthToken.RefreshToken
